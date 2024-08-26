efc070eb0bcb24da9984540ea6c102b87c877296
SPRV <sangprovai14@gmail.com>
2024-08-26 20:22:45 +0700

    config spring security

---
+++ src/main/java/com/dutact/web/configuration/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.dutact.web.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().permitAll()
+        );
+
+        return http.build();
+    }
+}
