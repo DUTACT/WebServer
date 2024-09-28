fb806e635d4dd5ae8b85be827f399eadda8c7b7b
Trần Sĩ  Tiến <106511449+TranSiTien@users.noreply.github.com>
2024-09-28 14:05:08 +0700

    chorce: optimize import

--- src/main/java/com/dutact/web/auth/dto/student/StudentRegisterDto.java
+++ src/main/java/com/dutact/web/auth/dto/student/StudentRegisterDto.java
@@ -1,7 +1,6 @@
 package com.dutact.web.auth.dto.student;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
