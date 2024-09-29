eafeec24328247d3c731168f78dbdef4820f1378
SPRV <sangprovai14@gmail.com>
2024-09-29 19:35:34 +0700

    ACT-52 fix swagger authen not working

--- src/main/java/com/dutact/web/configuration/OpenAPIDocConfiguration.java
+++ src/main/java/com/dutact/web/configuration/OpenAPIDocConfiguration.java
@@ -1,10 +1,22 @@
 package com.dutact.web.configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
+@OpenAPIDefinition(
+        info = @Info(title = "DUTACT API", version = "v69"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT")
 public class OpenAPIDocConfiguration {
 }
