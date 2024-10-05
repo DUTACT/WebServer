62c48adf3bdc934c1211b7aa292a81da1babff80
Trần Sĩ  Tiến <106511449+TranSiTien@users.noreply.github.com>
2024-10-05 19:23:42 +0700

    chorce: reformat sql script

--- src/main/resources/db/migration/V241005_01__create_post_table.sql
+++ src/main/resources/db/migration/V241005_01__create_post_table.sql
@@ -2,12 +2,12 @@ BEGIN;
 
 CREATE TABLE "post"
 (
-    id                    SERIAL PRIMARY KEY,
-    content               TEXT                        NOT NULL,
-    posted_at              TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    cover_photo_url       VARCHAR(1023)               NOT NULL,
-    status                jsonb                       NOT NULL,
-    event_id          INTEGER                     NOT NULL
+    id                  SERIAL PRIMARY KEY,
+    content             TEXT                        NOT NULL,
+    posted_at           TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    cover_photo_url     VARCHAR(1023)               NOT NULL,
+    status              jsonb                       NOT NULL,
+    event_id            INTEGER                     NOT NULL
 );
 
 CREATE INDEX idx_post_status_type ON "post" ((status ->> 'type'));
