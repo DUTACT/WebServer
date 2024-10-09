e8405589f792cd316de945d75ae42f5fe2b51936
Sang Tấn <sangprovai14@gmail.com>
2024-10-09 21:55:25 +0700

    ACT-68 fix swagger (#18)

--- src/main/java/com/dutact/web/features/event/student/dtos/EventDto.java
+++ src/main/java/com/dutact/web/features/event/student/dtos/EventDto.java
@@ -1,11 +1,13 @@
 package com.dutact.web.features.event.student.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Schema(name = "StudentEventDto")
 public class EventDto {
     @JsonProperty("id")
     private Integer id;
