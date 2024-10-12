beba8e1da1ba3b06d8229cbd829737e45c1dd613
SPRV <sangprovai14@gmail.com>
2024-10-12 17:17:19 +0700

    ACT-79 return status code for login

--- src/main/java/com/dutact/web/auth/factors/AccountController.java
+++ src/main/java/com/dutact/web/auth/factors/AccountController.java
@@ -4,13 +4,15 @@ import com.dutact.web.auth.dto.LoginDto;
 import com.dutact.web.auth.dto.ResponseToken;
 import com.dutact.web.auth.exception.AccountNotEnabledException;
 import com.dutact.web.auth.exception.InvalidLoginCredentialsException;
-import com.dutact.web.common.api.ErrorMessage;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,15 +36,33 @@ public class AccountController {
             @ApiResponse(responseCode = "200", description = "Login successful",
                     content = @Content(schema = @Schema(implementation = ResponseToken.class))),
             @ApiResponse(responseCode = "401", description = "Unauthorized - Invalid credentials or account not enabled",
-                    content = @Content(schema = @Schema(implementation = ErrorMessage.class)))
+                    content = @Content(schema = @Schema(implementation = LoginFailedResponse.class)))
     })
     public ResponseEntity<Object> postLogin(@RequestBody @Valid LoginDto loginDto) {
         try {
             ResponseToken responseToken = accountService.login(loginDto);
             return ResponseEntity.ok(responseToken);
-        }
-        catch (InvalidLoginCredentialsException | AccountNotEnabledException e) {
-            return new ResponseEntity<>(new ErrorMessage(e.getMessage()), HttpStatus.UNAUTHORIZED);
+        } catch (InvalidLoginCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new LoginFailedResponse(LoginFailedStatus.INVALID_LOGIN_CREDENTIALS));
+        } catch (AccountNotEnabledException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new LoginFailedResponse(LoginFailedStatus.ACCOUNT_NOT_ENABLED));
         }
     }
+}
+
+class LoginFailedStatus {
+    public static final String INVALID_LOGIN_CREDENTIALS = "invalid_credentials";
+    public static final String ACCOUNT_NOT_ENABLED = "account_not_enabled";
+}
+
+@Data
+@AllArgsConstructor
+class LoginFailedResponse {
+    @Schema(allowableValues = {
+            LoginFailedStatus.INVALID_LOGIN_CREDENTIALS,
+            LoginFailedStatus.ACCOUNT_NOT_ENABLED})
+    @JsonProperty("status")
+    private String status;
 }
