4380fe0e330d13502863635c5b2b09fb49512b2e
SPRV <sangprovai14@gmail.com>
2024-11-03 13:51:14 +0700

    update endpoint

--- src/main/java/com/dutact/web/features/checkin/admin/controllers/EventCheckinCodeController.java
+++ src/main/java/com/dutact/web/features/checkin/admin/controllers/EventCheckinCodeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/api/admin/events/participation-codes")
+@RequestMapping("/api/admin/events/checkin-codes")
 public class EventCheckinCodeController {
     private final EventCheckInCodeService checkInCodeService;
 
