ba3f57618422aeb2dabcc72a4a1cae6bed94c4e9
Trần Sĩ  Tiến <tientransidh@gmail.com>
2024-11-07 09:39:46 +0700

    fix: response status code 500 when redeem incorrect password (#69)

--- src/main/java/com/dutact/web/features/profile/student/controllers/StudentProfileController.java
+++ src/main/java/com/dutact/web/features/profile/student/controllers/StudentProfileController.java
@@ -1,12 +1,20 @@
 package com.dutact.web.features.profile.student.controllers;
 
 import com.dutact.web.auth.dto.NewPasswordDto;
+import com.dutact.web.auth.dto.ResponseToken;
 import com.dutact.web.auth.exception.InvalidLoginCredentialsException;
 import com.dutact.web.auth.exception.NoPermissionException;
+import com.dutact.web.auth.factors.LoginFailedResponse;
+import com.dutact.web.common.api.ErrorMessage;
 import com.dutact.web.common.api.exceptions.ConflictException;
 import com.dutact.web.common.api.exceptions.NotExistsException;
 import com.dutact.web.features.profile.student.dtos.StudentProfileUpdateDto;
 import com.dutact.web.features.profile.student.service.StudentProfileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -34,8 +42,14 @@ public class StudentProfileController {
     }
 
     @PutMapping("/{id}/change-password")
-    public ResponseEntity<?> changePassword(@PathVariable Integer id, @RequestBody NewPasswordDto newPasswordDto) throws NotExistsException, InvalidLoginCredentialsException, NoPermissionException {
-        studentProfileService.changePassword(id, newPasswordDto);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<?> changePassword(@PathVariable Integer id, @RequestBody NewPasswordDto newPasswordDto) throws NotExistsException {
+        try {
+            studentProfileService.changePassword(id, newPasswordDto);
+            return ResponseEntity.ok().build();
+        } catch (InvalidLoginCredentialsException e) {
+            return ResponseEntity.badRequest().body(new ErrorMessage(e.getMessage()));
+        } catch (NoPermissionException e) {
+            return ResponseEntity.status(403).body(new ErrorMessage(e.getMessage()));
+        }
     }
 }
