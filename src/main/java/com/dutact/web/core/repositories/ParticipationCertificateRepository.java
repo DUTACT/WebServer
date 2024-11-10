51538980478665c81c243ee13482c3f975c9b9a5
Trần Sĩ  Tiến <106511449+TranSiTien@users.noreply.github.com>
2024-11-10 14:45:26 +0700

    refactor: move participation certificate to event registation

--- src/main/java/com/dutact/web/core/repositories/ParticipationCertificateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dutact.web.core.repositories;
-
-import com.dutact.web.core.entities.participationcert.ParticipationCertificate;
-import com.dutact.web.core.entities.participationcert.ParticipationCertificateId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParticipationCertificateRepository
-        extends JpaRepository<ParticipationCertificate, ParticipationCertificateId> {
-}
