5f6f809e9809e510d48e3f74442eb652eeffb97f
SPRV <sangprovai14@gmail.com>
2024-11-11 18:02:48 +0700

    ACT-260 update

--- src/main/java/com/dutact/web/features/checkin/admin/controllers/AdminEventParticipationController.java
+++ src/main/java/com/dutact/web/features/checkin/admin/controllers/AdminEventParticipationController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/admin/event-participation")
+@RequestMapping("/api/admin/events/{eventId}/participation")
 public class AdminEventParticipationController {
     @PostMapping("/confirm")
     public void confirm() {
