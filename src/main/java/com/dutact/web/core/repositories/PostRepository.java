2c1ae2f3289e66d502a3b47aba8e4db1b5593f5a
Trần Sĩ  Tiến <106511449+TranSiTien@users.noreply.github.com>
2024-11-14 02:50:22 +0700

    add sort by created time

--- src/main/java/com/dutact/web/core/repositories/PostRepository.java
+++ src/main/java/com/dutact/web/core/repositories/PostRepository.java
@@ -1,6 +1,7 @@
 package com.dutact.web.core.repositories;
 
 import com.dutact.web.core.entities.post.Post;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,7 @@ import java.util.Optional;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
     boolean existsByIdAndEventId(Integer postId, Integer eventId);
+    List<Post> findAllByEventId(Integer eventId, Sort sort);
     List<Post> findAllByEventId(Integer eventId);
     List<Post> findAllByEventIdIn(List<Integer> eventIds);
 
