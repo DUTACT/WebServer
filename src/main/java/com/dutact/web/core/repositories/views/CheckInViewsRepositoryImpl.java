8d2b9f864ea54c65955af1363094682f1f4fed56
SPRV <sangprovai14@gmail.com>
2024-11-16 11:54:54 +0700

    fix participation query

--- src/main/java/com/dutact/web/core/repositories/views/CheckInViewsRepositoryImpl.java
+++ src/main/java/com/dutact/web/core/repositories/views/CheckInViewsRepositoryImpl.java
@@ -4,9 +4,16 @@ import com.dutact.web.common.api.PageResponse;
 import com.dutact.web.core.entities.common.UploadedFile;
 import com.dutact.web.core.entities.eventregistration.participationcert.ParticipationCertificateStatus;
 import com.dutact.web.core.projections.CheckInPreview;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class CheckInViewsRepositoryImpl implements CheckInViewsRepository {
@@ -14,53 +21,79 @@ public class CheckInViewsRepositoryImpl implements CheckInViewsRepository {
     private EntityManager entityManager;
 
     @Override
+    @SuppressWarnings("unchecked")
     public PageResponse<CheckInPreview> getCheckInPreviews(CheckInQueryParams queryParams) {
-        var query = """
-                SELECT check_in_preview.studentId, check_in_preview.studentName,
-                       check_in_preview.studentAvatarUrl, check_in_preview.totalCheckIn,
-                       check_in_preview.certificateStatus
-                FROM (
-                    SELECT student.id as studentId, student.fullName as studentName,
-                           student.avatar as studentAvatarUrl,
-                           registration.certificateStatus as certificateStatus,
-                           COUNT(event_check_in.id) as totalCheckIn
-                    FROM EventRegistration registration
-                        JOIN Student student ON registration.student.id = student.id
-                        LEFT JOIN EventCheckIn event_check_in ON registration.student.id = event_check_in.student.id
-                        LEFT JOIN EventCheckInCode event_check_in_code ON event_check_in.checkInCode.id = event_check_in_code.id
-                    WHERE (:searchQuery IS NULL OR student.fullName LIKE :searchQuery)
-                        AND registration.event.id = :eventId
-                        AND (event_check_in_code.id IS NULL OR event_check_in_code.event.id = :eventId)
-                    GROUP BY student.id,
-                             student.fullName,
-                             student.avatar,
-                             registration.certificateStatus
-                    ORDER BY student.fullName
-                ) AS check_in_preview
+        var query = """          
+                WITH paged_registration AS (
+                                         SELECT
+                                             s.id AS student_id,
+                                             s.full_name AS student_name,
+                                             s.avatar AS student_avatar_url,
+                                             er.certificate_status AS certificate_status
+                                         FROM
+                                             event_registration er
+                                         JOIN
+                                             student s
+                                         ON
+                                             er.student_id = s.id
+                                         WHERE
+                                             (:searchQuery = '' OR s.full_name LIKE :searchQuery)
+                                             AND er.event_id = :eventId
+                                         ORDER BY
+                                             s.full_name
+                                     ),
+                                     check_in_counts AS (
+                                         SELECT
+                                             COUNT(ecc.id) AS total_check_ins,
+                                             eci.student_id
+                                         FROM
+                                             event_check_in eci
+                                         JOIN
+                                             event_checkin_code ecc
+                                         ON
+                                             ecc.id = eci.check_in_code_id
+                                         WHERE
+                                             ecc.event_id = :eventId
+                                             AND eci.student_id IN (SELECT student_id FROM paged_registration)
+                                         GROUP BY
+                                             eci.student_id
+                                     )
+                                     SELECT
+                                         pr.student_id,
+                                            pr.student_name,
+                                            pr.student_avatar_url,
+                                            pr.certificate_status,
+                                         COALESCE(cic.total_check_ins, 0) AS total_check_ins
+                                     FROM
+                                         paged_registration pr
+                                     LEFT JOIN
+                                         check_in_counts cic
+                                     ON
+                                         pr.student_id = cic.student_id;
                 """;
 
-        var objQueryResult = entityManager.createQuery(query, Object.class)
-                .setParameter("searchQuery", queryParams.getSearchQuery() == null ? null : "%" + queryParams.getSearchQuery() + "%")
+        var queryResultObject = (List<Object>) entityManager.createNativeQuery(query, Object.class)
+                .setParameter("searchQuery", queryParams.getSearchQuery() == null ? "" : "%" + queryParams.getSearchQuery() + "%")
                 .setParameter("eventId", queryParams.getEventId())
                 .setFirstResult((queryParams.getPage() - 1) * queryParams.getPageSize())
                 .setMaxResults(queryParams.getPageSize())
                 .getResultList();
 
-        var queryResult = objQueryResult.stream()
-                .map(obj -> {
-                    var arr = (Object[]) obj;
-                    UploadedFile avatar = (UploadedFile) arr[2];
-                    String avatarUrl = null;
-                    if (avatar != null) {
-                        avatarUrl = avatar.getFileUrl();
+        var objectMapper = new ObjectMapper();
+
+        var queryResult = queryResultObject.stream()
+                .map(result -> {
+                    try {
+                        var resultArray = (Object[]) result;
+                        var studentId = (Integer) resultArray[0];
+                        var studentName = (String) resultArray[1];
+                        var studentAvatar = objectMapper.readValue(resultArray[2].toString(), UploadedFile.class);
+                        var certificateStatus = objectMapper.readValue(resultArray[3].toString(), ParticipationCertificateStatus.class);
+                        var totalCheckIn = (Long) resultArray[4];
+                        return new CheckInPreview(studentId, studentName, studentAvatar.getFileUrl(), totalCheckIn, certificateStatus);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
                     }
-                    return new CheckInPreview(
-                            (Integer) arr[0],
-                            (String) arr[1],
-                            avatarUrl,
-                            (Long) arr[3],
-                            (ParticipationCertificateStatus) arr[4]
-                    );
                 })
                 .toList();
 
@@ -81,4 +114,24 @@ public class CheckInViewsRepositoryImpl implements CheckInViewsRepository {
                 queryParams.getPage(),
                 queryParams.getPageSize());
     }
+}
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+class CheckInCountResult {
+    private Long checkInCount;
+    private Integer studentId;
+}
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+class RegistrationResult {
+    private Integer studentId;
+    private String studentName;
+    private UploadedFile studentAvatarUrl;
+    private ParticipationCertificateStatus certificateStatus;
 }
