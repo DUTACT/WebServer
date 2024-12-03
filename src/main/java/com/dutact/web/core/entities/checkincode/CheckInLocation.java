176d1cf345cb92842bafaa6a1d9518643c384624
SPRV <sangprovai14@gmail.com>
2024-12-03 19:52:02 +0700

    ACT-360 update

--- src/main/java/com/dutact/web/core/entities/checkincode/CheckInLocation.java
+++ src/main/java/com/dutact/web/core/entities/checkincode/CheckInLocation.java
@@ -1,17 +1,12 @@
 package com.dutact.web.core.entities.checkincode;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.annotation.Nullable;
 import lombok.Data;
 
 @Data
 public class CheckInLocation {
     @JsonProperty("title")
     private String title;
-
-    @Nullable
-    @JsonProperty("address")
-    private String address;
 
     @JsonProperty("geoPosition")
     private GeoPosition geoPosition;
