9aa8dc7bc13335bb81c5b26f44da60e0516ee3cb
SPRV <sangprovai14@gmail.com>
2025-01-01 11:04:47 +0700

    ACT-438 mark all notifications as read

--- src/main/java/com/dutact/web/features/notification/core/service/NotificationService.java
+++ src/main/java/com/dutact/web/features/notification/core/service/NotificationService.java
@@ -5,8 +5,14 @@ import com.dutact.web.common.api.exceptions.NotExistsException;
 import com.dutact.web.features.notification.core.dto.NotificationDto;
 import com.dutact.web.features.notification.core.dto.NotificationQueryParams;
 
+import java.util.List;
+
 public interface NotificationService {
     PageResponse<NotificationDto> getNotifications(NotificationQueryParams queryParams);
 
     NotificationDto markAsRead(Integer notificationId) throws NotExistsException;
+
+    void markAllAsRead(Integer accountId);
+
+    void markAsRead(List<Integer> notificationIds);
 }
